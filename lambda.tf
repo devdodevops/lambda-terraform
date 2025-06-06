7742d832eaa23beb4cd0433f1273677dc0519e79
Dev Parmar <dev.parmar@rackspace.com>
2025-06-06 16:23:29 +0530

    update

--- lambda.tf
+++ lambda.tf
@@ -140,7 +140,7 @@ module "lambda" {
   function_name                = "export_logs_to_s3"
   function_handler             = "lambda_function.lambda_handler"
   function_runtime             = "python3.12"
-  lambda_iam_role              = module.lambda_iam_role.0.iam_role_arn
+  lambda_iam_role              = module.lambda_iam_role
   function_timeout_in_seconds  = 600
   function_source_dir          = "${path.module}/aws_lambda_functions/export_logs_to_s3"
   function_zip_output_dir      = "${path.module}/build"
